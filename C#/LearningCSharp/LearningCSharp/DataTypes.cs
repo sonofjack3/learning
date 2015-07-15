6b4119eccd32caaebdb08737813b6f7dc9376500
Evan Jackson <evanjamesjackson@gmail.com>
2015-07-15 15:30:58 -0300

    Moved files to Visual Studio Project; Created DataTypes

---
+++ C#/LearningCSharp/LearningCSharp/DataTypes.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*  2. DATA TYPES */
+
+/*	Classes in Java and C# can be thought of as compound data types, with fields, methods and events
+	In C#, a struct is a stack-allocated compound data type that does not support inheritance.
+	In most other respects, structs are very similar to classes, but are more lightweight than classes (better performance).
+*/
+
+namespace DataTypes
+{
+    public class Example
+    {
+        /*  C# allows for contants using the "const" keyword (same as "final" keyword in Java).
+            The "readonly" keyword allows a variable's value to never be changed.
+            Readonly variables are useful when modules that share data need to be compiled separately.
+        */
+        const int MAX = 20;
+        readonly string WHATEVER = "Whatever";
+
+        public enum Color
+        {
+            Green,      //defaults to 0
+            Orange,     //defaults to 1
+            Red,        //defaults to 2
+            Blue        //defaults to 3
+        }
+
+        [STAThread]
+        static void Main()
+        {
+            PrimitiveDataTypes();
+
+            EnumeratedTypes();
+
+            Strings();
+
+            Conversions();            
+        }
+
+        private static void PrimitiveDataTypes()
+        {
+            /*  C# provides all data types available in Java and a few more.
+                In Java, each primitive data type is wrapped in a core class (eg: double is wrapped by Double).
+                In C#, all primitive data types already are objects in the System namespace (eg: int is the alias for System.Int32).
+                It is thus possible to call methods on a primitive data type, like so:
+            */
+            int i = 10;
+            object o = i;
+            Console.WriteLine(o.ToString());
+        }
+
+        private static void EnumeratedTypes()
+        {
+            /*  C# supports enumerated types */
+
+            Console.WriteLine("Possible colors: ");
+ 
+            //Use the Enum.GetNames method to return a string array of named constants for an enum
+            foreach (string s in Enum.GetNames(typeof(Color)))
+            {
+                Console.WriteLine(s);
+            }
+
+            Color currentColor = Color.Orange;
+
+            Console.WriteLine("Current color is {0}", currentColor);
+            Console.WriteLine("Current color value is {0}", (int)currentColor);
+        }
+
+        private static void Strings()
+        {
+            /*  Strings in Java and C# are immutable.
+                C# Strings can be compared using the == and != operators (NICE).
+            */
+
+            if ("ECH" == "ECH")
+            {
+                Console.WriteLine("Those two are the same!");
+            }
+
+            /*  String literals can be used to avoid having to escape characters (precede with the @ symbol) */
+
+            string line = @"Well,    I'm not really sure about that \m/";
+            Console.WriteLine(line);
+        }
+
+        private static void Conversions()
+        {
+            /*  Implicit conversions are supported in C# */
+            int a = 5;
+            long b = a;
+            /*  Explicit conversions are also supported */
+            long c = 5483;
+            int d = (int)c;
+        }
+
+        private
+}
--- C#/LearningCSharp/LearningCSharp/SourceFileConventions.cs
+++ C#/LearningCSharp/LearningCSharp/SourceFileConventions.cs
@@ -1,16 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Server = Microsoft.SqlServer.Server;
+
+/* 1. SOURCE FILE CONVENTIONS */
+
 /*	In Java, classes can be grouped using the package keyword.
 	In C#, the concept of namespaces is used to group logically related classes.
 	This way, two classes with the same name might appear in two separate namespaces.
 */
 
-using System.IO;
-using Server = Microsoft.SqlServer.Server;
-
 namespace Acme
 {
-	class Customer
+    class Customer
     {
-
+        //
     }
 }
 
@@ -18,14 +24,14 @@ namespace Acme
 
 namespace AcmeAccounting
 {
-	public class GetDetails
+    class Customer
     {
-		//
+        //
     }
 }
 
 /*	Classes in other namespaces can be accessed using the fully qualified name for the class. 
-	This can get unwieldy, so often we use the "using" keyword to specify an alias for the class (see line 7).
+	This can get unwieldy, so often we use the "using" keyword to specify an alias for the class (see line 6).
 	The class can then be referenced using this alias (eg: Server.whatever)
 */
 
