a93bc058eb2dc430301ecd70a1d7761d28621425
Evan Jackson <evanjamesjackson@gmail.com>
2015-07-16 11:07:16 -0300

    Added Structs.cs, updated project settings.

---
+++ C#/LearningCSharp/LearningCSharp/Structs.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+/*  7. STRUCTS */
+
+/*  Structs can be thought of as lightweight classes */
+/*  Structs are mostly used to encapsulate groups of related fields */
+/*  Structs are value types and can thus be allocated slightly more efficiently. */
+/*  Structs cannot be abstract and don't support inheritance */
+
+namespace Structs
+{
+    public struct Person
+    {
+        public int ID;
+        public string Name;
+
+        public Person(int id, string name)
+        {
+            ID = id;
+            Name = name;
+        }
+    }
+
+    public class Example
+    {
+        public static void Main()
+        {
+            Person person = new Person();   //using default constructor
+
+            Console.WriteLine("Struct values before initialization:");
+            Console.WriteLine("ID = {0}, Name = {1}", person.ID, person.Name);
+            Console.WriteLine();
+
+            person.ID = 2134;
+            person.Name = "Evan";
+
+            Console.WriteLine("Struct values after initialization:");
+            Console.WriteLine("ID = {0}, Name = {1}", person.ID, person.Name);
+        }
+    }
+}
