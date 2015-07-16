0bd2769eb28d3a24e48985d2974382e5761a937a
Evan Jackson <evanjamesjackson@gmail.com>
2015-07-16 10:47:18 -0300

    Added Properties.cs

---
+++ C#/LearningCSharp/LearningCSharp/Properties.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+/*  6.  PROPERTIES */
+
+/*  In C#, a property is a named member of a class, struct or interface. */
+/*  "get" and "set" are default accessor methods for accessing properties. */
+
+namespace Properties
+{
+    public class Person
+    {
+        private string name;
+
+        public String Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+    }
+
+    public class Example
+    {
+        public static void Main()
+        {
+            Person person = new Person();
+            // To use the get/set methods, do the following:
+            person.Name = "Evan";               // set accessor
+            Console.WriteLine(person.Name);     // get accessor
+        }
+    }
+}
