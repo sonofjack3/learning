051957319b714439c5b6652c8a547b7c38418815
Evan Jackson <evanjamesjackson@gmail.com>
2015-07-16 16:31:19 -0300

    Added Attributes.cs and updated project settings

---
+++ C#/LearningCSharp/LearningCSharp/Attributes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Web.Services;
+
+/*  12. ATTRIBUTES */
+
+/*  Attributes are for adding declarative information about types.
+    .NET Framework attributes can be used to take care of background stuff when calling a method or using a class.
+*/
+
+namespace Attributes
+{
+    public class Utilities : System.Web.Services.WebService
+    {
+        [System.Web.Services.WebMethod] // Attribute
+        public string GetTime()
+        {
+            return DateTime.Now.ToShortTimeString();
+        }
+    }
+
+    public class Example
+    {
+        public static void Main()
+        {
+            Utilities utilities = new Utilities();
+            Console.WriteLine(utilities.GetTime());
+        }
+    }
+}
