c65b2d66b724cdc7859c49125560de6a77dfe4b1
Evan Jackson <evanjamesjackson@gmail.com>
2015-07-20 16:59:32 -0300

    Clicking buttons now add text to display

--- C#/Visual C# .NET/Calculator/Calculator/Form1.Designer.cs
+++ C#/Visual C# .NET/Calculator/Calculator/Form1.Designer.cs
@@ -70,6 +70,7 @@
             this.button2.TabIndex = 2;
             this.button2.Text = "2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -79,6 +80,7 @@
             this.button3.TabIndex = 3;
             this.button3.Text = "3";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -88,6 +90,7 @@
             this.button4.TabIndex = 4;
             this.button4.Text = "4";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
@@ -97,6 +100,7 @@
             this.button5.TabIndex = 5;
             this.button5.Text = "5";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
@@ -106,6 +110,7 @@
             this.button6.TabIndex = 6;
             this.button6.Text = "6";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button7
             // 
@@ -115,6 +120,7 @@
             this.button7.TabIndex = 7;
             this.button7.Text = "7";
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button8
             // 
@@ -124,6 +130,7 @@
             this.button8.TabIndex = 8;
             this.button8.Text = "8";
             this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // button9
             // 
@@ -133,6 +140,7 @@
             this.button9.TabIndex = 9;
             this.button9.Text = "9";
             this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
             // button0
             // 
@@ -142,6 +150,7 @@
             this.button0.TabIndex = 10;
             this.button0.Text = "0";
             this.button0.UseVisualStyleBackColor = true;
+            this.button0.Click += new System.EventHandler(this.button0_Click);
             // 
             // buttonDecimal
             // 
@@ -151,6 +160,7 @@
             this.buttonDecimal.TabIndex = 11;
             this.buttonDecimal.Text = ".";
             this.buttonDecimal.UseVisualStyleBackColor = true;
+            this.buttonDecimal.Click += new System.EventHandler(this.buttonDecimal_Click);
             // 
             // buttonAdd
             // 
--- C#/Visual C# .NET/Calculator/Calculator/Form1.cs
+++ C#/Visual C# .NET/Calculator/Calculator/Form1.cs
@@ -17,9 +17,59 @@ namespace Calculator
             InitializeComponent();
         }
 
+        private void button0_Click(object sender, EventArgs e)
+        {
+            displayTextBox.Text += "0";
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
+            displayTextBox.Text += "1";
+        }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            displayTextBox.Text += "2";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            displayTextBox.Text += "3";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            displayTextBox.Text += "4";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            displayTextBox.Text += "5";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            displayTextBox.Text += "6";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            displayTextBox.Text += "7";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            displayTextBox.Text += "8";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            displayTextBox.Text += "9";
+        }
+
+        private void buttonDecimal_Click(object sender, EventArgs e)
+        {
+            displayTextBox.Text += ".";
         }
     }
 }
