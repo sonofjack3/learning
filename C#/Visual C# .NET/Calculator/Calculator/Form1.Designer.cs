5ab459c4eb4fff14b7e019ff83f859ee1fe6f56d
Evan Jackson <evanjamesjackson@gmail.com>
2015-07-20 16:49:36 -0300

    Resized entire form

--- C#/Visual C# .NET/Calculator/Calculator/Form1.Designer.cs
+++ C#/Visual C# .NET/Calculator/Calculator/Form1.Designer.cs
@@ -183,7 +183,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 349);
+            this.ClientSize = new System.Drawing.Size(239, 272);
             this.Controls.Add(this.buttonClear);
             this.Controls.Add(this.buttonEquals);
             this.Controls.Add(this.buttonAdd);
