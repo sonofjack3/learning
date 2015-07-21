a1a63921570e5f0beceed9400b17bb620b1a773d
Evan Jackson <evanjamesjackson@gmail.com>
2015-07-21 15:57:42 -0300

    Added underlines to file menu; added shortcuts

--- C#/VisualC#.NET/Menus/Menus/Form1.Designer.cs
+++ C#/VisualC#.NET/Menus/Menus/Form1.Designer.cs
@@ -60,7 +60,7 @@
             this.quitToolStripMenuItem});
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
             this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
-            this.fileToolStripMenuItem.Text = "File";
+            this.fileToolStripMenuItem.Text = "&File";
             // 
             // newToolStripMenuItem
             // 
