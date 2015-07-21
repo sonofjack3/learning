bbd6135ae6cee0713ca76f4e6d433b93e1937305
Evan Jackson <evanjamesjackson@gmail.com>
2015-07-21 15:12:07 -0300

    Added Multiply By button

--- C#/VisualC#.NET/ListBoxes/ListBoxes/Form1.Designer.cs
+++ C#/VisualC#.NET/ListBoxes/ListBoxes/Form1.Designer.cs
@@ -34,12 +34,14 @@
             this.textBoxStart = new System.Windows.Forms.TextBox();
             this.textBoxEnd = new System.Windows.Forms.TextBox();
             this.buttonLoop = new System.Windows.Forms.Button();
+            this.labelMultiplicand = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // listBoxResults
             // 
             this.listBoxResults.FormattingEnabled = true;
-            this.listBoxResults.Location = new System.Drawing.Point(31, 104);
+            this.listBoxResults.Location = new System.Drawing.Point(31, 136);
             this.listBoxResults.Name = "listBoxResults";
             this.listBoxResults.Size = new System.Drawing.Size(228, 95);
             this.listBoxResults.TabIndex = 0;
@@ -78,7 +80,7 @@
             // 
             // buttonLoop
             // 
-            this.buttonLoop.Location = new System.Drawing.Point(207, 22);
+            this.buttonLoop.Location = new System.Drawing.Point(207, 43);
             this.buttonLoop.Name = "buttonLoop";
             this.buttonLoop.Size = new System.Drawing.Size(52, 36);
             this.buttonLoop.TabIndex = 5;
@@ -86,11 +88,29 @@
             this.buttonLoop.UseVisualStyleBackColor = true;
             this.buttonLoop.Click += new System.EventHandler(this.buttonLoop_Click);
             // 
+            // labelMultiplicand
+            // 
+            this.labelMultiplicand.AutoSize = true;
+            this.labelMultiplicand.Location = new System.Drawing.Point(28, 85);
+            this.labelMultiplicand.Name = "labelMultiplicand";
+            this.labelMultiplicand.Size = new System.Drawing.Size(57, 13);
+            this.labelMultiplicand.TabIndex = 6;
+            this.labelMultiplicand.Text = "Multiply By";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(87, 82);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(114, 20);
+            this.textBox1.TabIndex = 7;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelMultiplicand);
             this.Controls.Add(this.buttonLoop);
             this.Controls.Add(this.textBoxEnd);
             this.Controls.Add(this.textBoxStart);
@@ -112,6 +132,8 @@
         private System.Windows.Forms.TextBox textBoxStart;
         private System.Windows.Forms.TextBox textBoxEnd;
         private System.Windows.Forms.Button buttonLoop;
+        private System.Windows.Forms.Label labelMultiplicand;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
