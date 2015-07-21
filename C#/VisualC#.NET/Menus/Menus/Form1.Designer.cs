4d925c150101e44cfb986c496885cc6b34438651
Evan Jackson <evanjamesjackson@gmail.com>
2015-07-21 16:18:15 -0300

    Added two new text boxes

--- C#/VisualC#.NET/Menus/Menus/Form1.Designer.cs
+++ C#/VisualC#.NET/Menus/Menus/Form1.Designer.cs
@@ -46,6 +46,8 @@
             this.viewTextBoxesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.viewLabelsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.viewImagesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -194,11 +196,29 @@
             this.viewImagesToolStripMenuItem.Size = new System.Drawing.Size(230, 22);
             this.viewImagesToolStripMenuItem.Text = "View &Images";
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(24, 61);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(96, 92);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(146, 61);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(96, 92);
+            this.textBox2.TabIndex = 2;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
@@ -230,6 +250,8 @@
         private System.Windows.Forms.ToolStripMenuItem viewTextBoxesToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem viewLabelsToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem viewImagesToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
