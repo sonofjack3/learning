b76210a7a6df022a3f0e02a620726be0bc4fb893
Evan Jackson <evanjamesjackson@gmail.com>
2015-07-23 16:56:00 -0300

    Added image list, added back, forward, reload, stop and home buttons and logic

--- C#/VisualC#.NET/CustomWebBrowser/CustomWebBrowser/Form1.Designer.cs
+++ C#/VisualC#.NET/CustomWebBrowser/CustomWebBrowser/Form1.Designer.cs
@@ -28,11 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.webBrowser1 = new System.Windows.Forms.WebBrowser();
             this.textBoxAddress = new System.Windows.Forms.TextBox();
             this.buttonGo = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.buttonBack = new System.Windows.Forms.Button();
+            this.buttonForward = new System.Windows.Forms.Button();
+            this.buttonReload = new System.Windows.Forms.Button();
+            this.buttonHome = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.SuspendLayout();
@@ -53,7 +61,6 @@
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Size = new System.Drawing.Size(524, 241);
             this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // webBrowser1
@@ -66,9 +73,9 @@
             // 
             // textBoxAddress
             // 
-            this.textBoxAddress.Location = new System.Drawing.Point(16, 14);
+            this.textBoxAddress.Location = new System.Drawing.Point(222, 14);
             this.textBoxAddress.Name = "textBoxAddress";
-            this.textBoxAddress.Size = new System.Drawing.Size(478, 20);
+            this.textBoxAddress.Size = new System.Drawing.Size(272, 20);
             this.textBoxAddress.TabIndex = 1;
             // 
             // buttonGo
@@ -81,11 +88,86 @@
             this.buttonGo.UseVisualStyleBackColor = true;
             this.buttonGo.Click += new System.EventHandler(this.buttonGo_Click);
             // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "back.png");
+            this.imageList1.Images.SetKeyName(1, "forward.png");
+            this.imageList1.Images.SetKeyName(2, "refresh.png");
+            this.imageList1.Images.SetKeyName(3, "stop.png");
+            this.imageList1.Images.SetKeyName(4, "home.png");
+            // 
+            // buttonBack
+            // 
+            this.buttonBack.ImageIndex = 0;
+            this.buttonBack.ImageList = this.imageList1;
+            this.buttonBack.Location = new System.Drawing.Point(12, 5);
+            this.buttonBack.Name = "buttonBack";
+            this.buttonBack.Padding = new System.Windows.Forms.Padding(4);
+            this.buttonBack.Size = new System.Drawing.Size(36, 36);
+            this.buttonBack.TabIndex = 3;
+            this.buttonBack.UseVisualStyleBackColor = true;
+            this.buttonBack.Click += new System.EventHandler(this.buttonBack_Click);
+            // 
+            // buttonForward
+            // 
+            this.buttonForward.ImageIndex = 1;
+            this.buttonForward.ImageList = this.imageList1;
+            this.buttonForward.Location = new System.Drawing.Point(54, 5);
+            this.buttonForward.Name = "buttonForward";
+            this.buttonForward.Padding = new System.Windows.Forms.Padding(4);
+            this.buttonForward.Size = new System.Drawing.Size(36, 36);
+            this.buttonForward.TabIndex = 4;
+            this.buttonForward.UseVisualStyleBackColor = true;
+            this.buttonForward.Click += new System.EventHandler(this.buttonForward_Click);
+            // 
+            // buttonReload
+            // 
+            this.buttonReload.ImageIndex = 2;
+            this.buttonReload.ImageList = this.imageList1;
+            this.buttonReload.Location = new System.Drawing.Point(96, 5);
+            this.buttonReload.Name = "buttonReload";
+            this.buttonReload.Padding = new System.Windows.Forms.Padding(4);
+            this.buttonReload.Size = new System.Drawing.Size(36, 36);
+            this.buttonReload.TabIndex = 5;
+            this.buttonReload.UseVisualStyleBackColor = true;
+            this.buttonReload.Click += new System.EventHandler(this.buttonReload_Click);
+            // 
+            // buttonHome
+            // 
+            this.buttonHome.ImageIndex = 4;
+            this.buttonHome.ImageList = this.imageList1;
+            this.buttonHome.Location = new System.Drawing.Point(180, 4);
+            this.buttonHome.Name = "buttonHome";
+            this.buttonHome.Padding = new System.Windows.Forms.Padding(4);
+            this.buttonHome.Size = new System.Drawing.Size(36, 36);
+            this.buttonHome.TabIndex = 6;
+            this.buttonHome.UseVisualStyleBackColor = true;
+            this.buttonHome.Click += new System.EventHandler(this.buttonHome_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.ImageIndex = 3;
+            this.buttonStop.ImageList = this.imageList1;
+            this.buttonStop.Location = new System.Drawing.Point(138, 5);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Padding = new System.Windows.Forms.Padding(4);
+            this.buttonStop.Size = new System.Drawing.Size(36, 36);
+            this.buttonStop.TabIndex = 7;
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(556, 321);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonHome);
+            this.Controls.Add(this.buttonReload);
+            this.Controls.Add(this.buttonForward);
+            this.Controls.Add(this.buttonBack);
             this.Controls.Add(this.buttonGo);
             this.Controls.Add(this.textBoxAddress);
             this.Controls.Add(this.tabControl1);
@@ -105,6 +187,12 @@
         private System.Windows.Forms.WebBrowser webBrowser1;
         private System.Windows.Forms.TextBox textBoxAddress;
         private System.Windows.Forms.Button buttonGo;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button buttonBack;
+        private System.Windows.Forms.Button buttonForward;
+        private System.Windows.Forms.Button buttonReload;
+        private System.Windows.Forms.Button buttonHome;
+        private System.Windows.Forms.Button buttonStop;
     }
 }
 
--- C#/VisualC#.NET/CustomWebBrowser/CustomWebBrowser/Form1.cs
+++ C#/VisualC#.NET/CustomWebBrowser/CustomWebBrowser/Form1.cs
@@ -12,15 +12,49 @@ namespace CustomWebBrowser
 {
     public partial class Form1 : Form
     {
+        private string home = "http://www.google.ca";
+
         public Form1()
         {
             InitializeComponent();
+            webBrowser1.Navigate(home);
         }
 
         private void buttonGo_Click(object sender, EventArgs e)
         {
             string address = textBoxAddress.Text.Trim();
             webBrowser1.Navigate(address);
+        }
+
+        private void buttonBack_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.CanGoBack)
+            {
+                webBrowser1.GoBack();
+            }
+        }
+
+        private void buttonForward_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.CanGoForward)
+            {
+                webBrowser1.GoForward();
+            }
+        }
+
+        private void buttonReload_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Refresh();
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Stop();
+        }
+
+        private void buttonHome_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate(home);
         }
     }
 }
