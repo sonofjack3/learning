9770461823fa95ab7250bf115e8949973da9d2ab
Evan Jackson <evanjamesjackson@gmail.com>
2015-07-23 13:53:00 -0300

    Added button click event handler stuff

--- C#/VisualC#.NET/Events/Events/Form1.cs
+++ C#/VisualC#.NET/Events/Events/Form1.cs
@@ -16,5 +16,14 @@ namespace Events
         {
             InitializeComponent();
         }
+
+        /*  This method is an event handler and handles the event of button1 being clicked */
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*  "sender" indicates which object sent the event */
+            MessageBox.Show("Sender object: " + sender.ToString());
+            /*  "e" is an object of the EventArgs class and indicates what event has been raised */
+            MessageBox.Show("EventArgs: " + e.ToString());
+        }
     }
 }
