04368f68691789dbac56e47c7f042b7853ae8b30
Evan Jackson <evanjamesjackson@gmail.com>
2015-07-24 09:46:23 -0300

    Deleted test1.txt; added logic to open button (opens file and reads text into text box)

--- C#/VisualC#.NET/ManipulatingFiles/ManipulatingFiles/Form1.Designer.cs
+++ C#/VisualC#.NET/ManipulatingFiles/ManipulatingFiles/Form1.Designer.cs
@@ -28,12 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Location = new System.Drawing.Point(102, 42);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpen.TabIndex = 0;
+            this.buttonOpen.Text = "Open";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(77, 97);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(126, 72);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonOpen);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- C#/VisualC#.NET/ManipulatingFiles/ManipulatingFiles/Form1.cs
+++ C#/VisualC#.NET/ManipulatingFiles/ManipulatingFiles/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace ManipulatingFiles
 {
@@ -15,6 +16,19 @@ namespace ManipulatingFiles
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            //This assumes there is a file on the computer Desktop called "test1.txt"
+            string fileName = "\\test1.txt";
+            fileName = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + fileName;
+            //An object of the StreamReader class can be used to read from a text file
+            StreamReader reader = new StreamReader(fileName);
+            //The ReadToEnd() method of a StreamReader object reads all contents of a file
+            textBox1.Text = reader.ReadToEnd();
+            //Remember to always close the stream (also don't cross them)
+            reader.Close();
         }
     }
 }
