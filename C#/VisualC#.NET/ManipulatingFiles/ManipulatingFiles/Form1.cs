4d20fa405be959ce63bece3178d6b84c1752ce09
Evan Jackson <evanjamesjackson@gmail.com>
2015-07-24 09:52:00 -0300

    Added try catch block for missing file

--- C#/VisualC#.NET/ManipulatingFiles/ManipulatingFiles/Form1.cs
+++ C#/VisualC#.NET/ManipulatingFiles/ManipulatingFiles/Form1.cs
@@ -23,12 +23,18 @@ namespace ManipulatingFiles
             //This assumes there is a file on the computer Desktop called "test1.txt"
             string fileName = "\\test1.txt";
             fileName = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + fileName;
-            //An object of the StreamReader class can be used to read from a text file
-            StreamReader reader = new StreamReader(fileName);
-            //The ReadToEnd() method of a StreamReader object reads all contents of a file
-            textBox1.Text = reader.ReadToEnd();
-            //Remember to always close the stream (also don't cross them)
-            reader.Close();
+            try {
+                //An object of the StreamReader class can be used to read from a text file
+                StreamReader reader = new StreamReader(fileName);
+                //The ReadToEnd() method of a StreamReader object reads all contents of a file
+                textBox1.Text = reader.ReadToEnd();
+                //Remember to always close the stream (also don't cross them)
+                reader.Close();
+            }
+            catch (FileNotFoundException fe)
+            {
+                textBox1.Text = "No file test1.txt on Desktop!";
+            }
         }
     }
 }
