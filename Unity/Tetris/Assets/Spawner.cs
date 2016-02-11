db18db55fd42f6a6589924f42e20e0b7943c607b
Evan Jackson <evanjamesjackson@gmail.com>
2016-02-10 22:08:41 -0400

    Added Spawner.cs

---
+++ Unity/Tetris/Assets/Spawner.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject[] tetrominoes;
+
+	// Use this for initialization
+	void Start () {
+		spawnNext ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void spawnNext() {
+		int i = Random.Range (0, tetrominoes.Length);
+
+		Instantiate (tetrominoes [i], transform.position, Quaternion.identity);
+	}
+}
