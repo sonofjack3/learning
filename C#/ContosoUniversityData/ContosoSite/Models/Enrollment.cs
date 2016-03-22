fd1cfc3196fbb2f9ab0b8e0ee8eb6e2594d53e8b
Evan Jackson <evanjamesjackson@gmail.com>
2016-03-22 16:19:13 -0300

    Added range constraint annotation to Grade property in Enrollment

--- C#/ContosoUniversityData/ContosoSite/Models/Enrollment.cs
+++ C#/ContosoUniversityData/ContosoSite/Models/Enrollment.cs
@@ -11,10 +11,12 @@ namespace ContosoSite.Models
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
     
     public partial class Enrollment
     {
         public int EnrollmentID { get; set; }
+        [Range(0, 4)]
         public Nullable<decimal> Grade { get; set; }
         public int CourseID { get; set; }
         public int StudentID { get; set; }
