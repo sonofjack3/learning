aa844193737291e5cf9da5563fb1357e0e69d765
Evan Jackson <evanjamesjackson@gmail.com>
2016-03-22 16:26:41 -0300

    Added PartialClasses.cs; updated Model files to use partial class properties instead

--- C#/ContosoUniversityData/ContosoSite/Models/Enrollment.cs
+++ C#/ContosoUniversityData/ContosoSite/Models/Enrollment.cs
@@ -11,12 +11,10 @@ namespace ContosoSite.Models
 {
     using System;
     using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
     
     public partial class Enrollment
     {
         public int EnrollmentID { get; set; }
-        [Range(0, 4)]
         public Nullable<decimal> Grade { get; set; }
         public int CourseID { get; set; }
         public int StudentID { get; set; }
--- C#/ContosoUniversityData/ContosoSite/Models/Student.cs
+++ C#/ContosoUniversityData/ContosoSite/Models/Student.cs
@@ -11,7 +11,6 @@ namespace ContosoSite.Models
 {
     using System;
     using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
     
     public partial class Student
     {
@@ -22,12 +21,9 @@ namespace ContosoSite.Models
         }
     
         public int StudentID { get; set; }
-        [StringLength(50)]
         public string LastName { get; set; }
-        [StringLength(50)]
         public string FirstName { get; set; }
         public Nullable<System.DateTime> EnrollmentDate { get; set; }
-        [StringLength(50)]
         public string MiddleName { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
