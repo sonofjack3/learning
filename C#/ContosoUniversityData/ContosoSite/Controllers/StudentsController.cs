4ba560c818045ad0e6e119bfdf635362bc605a57
Evan Jackson <evanjamesjackson@gmail.com>
2016-03-23 11:57:55 -0300

    Added error message displaying for deleting of students

--- C#/ContosoUniversityData/ContosoSite/Controllers/StudentsController.cs
+++ C#/ContosoUniversityData/ContosoSite/Controllers/StudentsController.cs
@@ -110,11 +110,15 @@ namespace ContosoSite.Controllers
         }
 
         // GET: Students/Delete/5
-        public ActionResult Delete(int? id)
+        public ActionResult Delete(int? id, bool? saveChangesError=false)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            if (saveChangesError.GetValueOrDefault())
+            {
+                ViewBag.ErrorMessage = "Delete failed. Try again.";
             }
             Student student = db.Students.Find(id);
             if (student == null)
@@ -125,13 +129,21 @@ namespace ContosoSite.Controllers
         }
 
         // POST: Students/Delete/5
-        [HttpPost, ActionName("Delete")]
+        [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
+        public ActionResult Delete(int id)
         {
-            Student student = db.Students.Find(id);
-            db.Students.Remove(student);
-            db.SaveChanges();
+            try
+            {
+                Student student = db.Students.Find(id);
+                db.Students.Remove(student);
+                db.SaveChanges();
+            }
+            catch (DataException/* dex */)
+            {
+                //Log the error (uncomment dex variable name and add a line here to write a log.
+                return RedirectToAction("Delete", new { id = id, saveChangesError = true });
+            }
             return RedirectToAction("Index");
         }
 
