f3cfaef5705bfed6e1ebe99a45cf0f675ee9d7c4
evanjamesjackson <evanjamesjackson@gmail.com>
2016-06-26 19:38:42 -0300

    Ball moves and bounces straight between rackets

---
+++ Unity/Pong/Assets/MoveBall.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBall : MonoBehaviour {
+    private float speed = 30;
+
+	// Use this for initialization
+	void Start () {
+        // Initial velocity
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+	}
+}
