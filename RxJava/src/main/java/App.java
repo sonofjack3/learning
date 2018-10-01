f6307966bdcff3347f994a2c35c71ea98c3d8ea1
evanjamesjackson <evanjamesjackson@gmail.com>
2018-09-30 22:45:45 -0300

    Renamed fakeUserInput to justExample and added method mapExample

--- RxJava/src/main/java/App.java
+++ RxJava/src/main/java/App.java
@@ -1,11 +1,26 @@
+import java.util.Random;
+
 import io.reactivex.*;
 
 public class App {
     public static void main(String[] args) {
-        fakeUserInput().subscribe(line -> System.out.println(line));
+        justExample().subscribe(line -> System.out.println(line));
+        mapExample().subscribe(line -> System.out.println(line));
     }
 
-    public static Observable<Integer> fakeUserInput() {
+    public static Observable<Integer> justExample() {
+        /* An Observable represents a stream of data.
+         * The just method takes in a list of data, and returns an Observable stream of that data.
+         */
         return Observable.just(10, 17, 3, 8);
+    }
+
+    public static Observable<Integer> mapExample() {
+        Random random = new Random();
+        /**
+         * The map method takes in a lambda, and will apply it to each element in the Observable's
+         * stream, outputting a new Observable with a "mapped" stream of data.
+         */
+        return justExample().map(number -> number * random.nextInt(20));
     }
 }
