82a4b75d746187119ee883608650d593f475fd1f
evanjamesjackson <evanjamesjackson@gmail.com>
2018-09-30 22:23:46 -0300

    Created method that returns an observable stream of "random" numbers

--- RxJava/src/main/java/App.java
+++ RxJava/src/main/java/App.java
@@ -2,6 +2,10 @@ import io.reactivex.*;
 
 public class App {
     public static void main(String[] args) {
-        Observable.just("Hello world").subscribe(line -> System.out.println(line));
+        fakeUserInput().subscribe(line -> System.out.println(line));
+    }
+
+    public static Observable<Integer> fakeUserInput() {
+        return Observable.just(10, 17, 3, 8);
     }
 }
