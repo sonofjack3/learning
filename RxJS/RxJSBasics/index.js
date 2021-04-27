5d8c7cdec049f64b4bbd1b4d6c5e1aca7558f70b
Evan Jackson <evan@jacksonnet.ca>
2021-04-27 16:09:24 -0300

    Demonstrating Observables

--- RxJS/RxJSBasics/index.js
+++ RxJS/RxJSBasics/index.js
@@ -1,8 +1,32 @@
-import { of } from 'rxjs';
+// Demonstrating how Observables work
+import { Observable } from "rxjs";
 
-/*
- * Any code samples you want to play with can go in this file.
- * Updates will trigger a live reload on http://localhost:1234/
- * after running npm start.
- */
-of('Hello', 'RxJS').subscribe(console.log);
+// Observables provide the ability to asynchronously "emit" or "push" events to items that need to react to those events
+
+// A new Observable accepts a function which accepts a subscriber, which it will call when it has completed its operation.
+// The "subscriber", then, is an object that "observes" the observer and reacts to its events.
+const observable = new Observable((subscriber) => {
+  // Calling next "pushes" (aka "emits") a value to the subscriber
+  subscriber.next("Hello");
+  subscriber.next("World");
+  subscriber.complete();
+});
+
+// A Subscriber is an object that reacts to events pushed from an Observer.
+// A subscriber is a "wrapped, safe version" of an observer. They can be more-or-less thought of as the same thing.
+const subscriber = {
+  // A subscriber/observer needs 3 "callback" functions
+
+  // next is for reacting to normal events emitted from the observable
+  next: (value) => console.log("next", value),
+
+  // error is for reacting to errors emitted by the observable
+  error: (error) => console.log("error", error),
+
+  // complete is for reacting to the observable completing it's operation. Once complete is called by an observable,
+  // the subscriber won't accept/react to any more events.
+  complete: () => console.log("complete!"),
+};
+
+// subscribe hooks the observer/subscriber to the observable and triggers the observable to emit its events
+observable.subscribe(observer);
